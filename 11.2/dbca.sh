396c1aa689e02bee55c771c989eb479210b72bf3
henryliu18 <45472005+henryliu18@users.noreply.github.com>
2019-05-10 21:55:25 +1000

    Update dbca.sh

--- 11.2/dbca.sh
+++ 11.2/dbca.sh
@@ -5,14 +5,18 @@
 # Database creation, run as oracle user
 #
 
-ORATAB=/etc/oratab
-TMPORATAB=/tmp/oratab
-
+# Source env
+if [ -f ./env ]; then
+ . ./env
+else
+ echo "env file not found, run setup to create env file"
+ exit 1
+fi
 
 dbca -silent -createDatabase -templateName General_Purpose.dbc -gdbname $ORACLE_SID -sid $ORACLE_SID -responseFile NO_VALUE -characterSet AL32UTF8 -memoryPercentage 10 -emConfiguration NONE -datafiledestination /ora/db001 -sysPassword SysPassword1 -systemPassword SysPassword1 -dbsnmpPassword SysPassword1 -sysmanPassword SysPassword1
 
 # Change auto start flag from N to Y
-sed -e "/$ORACLE_SID/s/^/#/g" $ORATAB > $TMPORATAB
-grep "$ORACLE_SID.*:N" $ORATAB | sed s'/..$/:Y/' >> $TMPORATAB
-cat $TMPORATAB $ORATAB
-rm -f $TMPORATAB
+sed -e "/$CDB/s/^/#/g" $ORATAB > ${SCRIPT_DIR}/tmporatab
+grep "$CDB.*:N" $ORATAB | sed s'/..$/:Y/' >> ${SCRIPT_DIR}/tmporatab
+cat ${SCRIPT_DIR}/tmporatab > $ORATAB
+rm -f ${SCRIPT_DIR}/tmporatab
