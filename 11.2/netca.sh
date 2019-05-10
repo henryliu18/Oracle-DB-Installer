19c48d75b49218687230ff669d802b3d9f98504e
henryliu18 <45472005+henryliu18@users.noreply.github.com>
2019-05-10 21:49:39 +1000

    Update netca.sh

--- 11.2/netca.sh
+++ 11.2/netca.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/bash
 
 #
-# Tested CentOS Linux release 7.6.1810 (Core)
-# Listener configuration, run as oracle user
+# Tested CentOS 7
+# Listener configuration, run as root user
 #
 
-netca /silent /responsefile $ORACLE_HOME/assistants/netca/netca.rsp
+# Source env
+if [ -f ./env ]; then
+ . ./env
+else
+ echo "env file not found, run setup to create env file"
+ exit 1
+fi
+
+echo "netca /silent /responsefile \$ORACLE_HOME/assistants/netca/netca.rsp" > ${SCRIPT_DIR}/run_netca
+chmod a+x ${SCRIPT_DIR}/run_netca
+su - $O_USER -c ${SCRIPT_DIR}/run_netca
+rm -f ${SCRIPT_DIR}/run_netca
