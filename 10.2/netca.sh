9864b80c94e915754854cda5480a959ba764848b
henryliu18 <45472005+henryliu18@users.noreply.github.com>
2019-05-13 02:20:31 +1000

    Create netca.sh

---
+++ 10.2/netca.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#
+# Tested CentOS 5
+# Listener configuration, run as root user
+#
+
+# Source env
+if [ -f ./env ]; then
+ . ./env
+else
+ echo "env file not found, run setup to create env file"
+ exit 1
+fi
+
+echo "netca /silent /responsefile \$ORACLE_HOME/network/install/netca_typ.rsp" > ${SCRIPT_DIR}/run_netca
+chmod a+x ${SCRIPT_DIR}/run_netca
+su - $O_USER -c ${SCRIPT_DIR}/run_netca
+rm -f ${SCRIPT_DIR}/run_netca
