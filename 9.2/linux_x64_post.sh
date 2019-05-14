1e44a4d5dd48fac92b13bce52c60379cf853a047
henryliu18 <45472005+henryliu18@users.noreply.github.com>
2019-05-14 22:13:14 +1000

    Delete linux_x64_post.sh

--- 9.2/linux_x64_post.sh
+++
@@ -1,8 +0,0 @@
-# Revert gcc 3.4 from gcc 3.2
-mv /usr/bin/gcc /usr/bin/gcc32
-mv /usr/bin/gcc34 /usr/bin/gcc
-
-# Cleanup
-rm -rf /tmp/ora9i
-rm -f /tmp/inst_ora_sw.sh
-rm -f /tmp/amd64_db_9204_Disk*
