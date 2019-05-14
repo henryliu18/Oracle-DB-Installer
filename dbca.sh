539e506332abfbb0fd59ae6be6a26cd63e7d3898
henryliu18 <45472005+henryliu18@users.noreply.github.com>
2019-05-14 21:09:21 +1000

    Update dbca.sh

--- dbca.sh
+++ dbca.sh
@@ -10,15 +10,15 @@ else
 fi
 
 if [ "$O_VER" = "18c" ]; then
-  bash 18c/dbca.sh
+  bash `dirname $0`/18c/dbca.sh
 elif [ "$O_VER" = "12c" ]; then
-  bash 12c/dbca.sh
+  bash `dirname $0`/12c/dbca.sh
 elif [ "$O_VER" = "11.2" ]; then
-  bash 11.2/dbca.sh
+  bash `dirname $0`/11.2/dbca.sh
 elif [ "$O_VER" = "10.2" ]; then
-  bash 10.2/dbca.sh
+  bash `dirname $0`/10.2/dbca.sh
 elif [ "$O_VER" = "9.2" ]; then
-  bash 9.2/dbca.sh
+  bash `dirname $0`/9.2/dbca.sh
 elif [ "$O_VER" = "8.1.7" ]; then
-  bash 8.1.7/dbca.sh
+  bash `dirname $0`/8.1.7/dbca.sh
 fi
