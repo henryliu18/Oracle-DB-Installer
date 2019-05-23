ee9aa25a4032777a1e37c95458b9ebcc32ee4d57
henryliu18 <45472005+henryliu18@users.noreply.github.com>
2019-05-23 19:54:44 +1000

    Create dbca.sh

---
+++ 12c/dbca.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#
+# Tested CentOS 7
+# Database creation, run as root user
+#
+
+# Source env
+if [ -f ./env ]; then
+ . ./env
+else
+ echo "env file not found, run setup to create env file"
+ exit 1
+fi
+
+echo "dbca -silent -createDatabase \
+     -templateName General_Purpose.dbc \
+     -gdbname \$ORACLE_SID \
+     -sid \$ORACLE_SID \
+     -responseFile NO_VALUE \
+     -characterSet AL32UTF8 \
+     -sysPassword "$SYS_PASS" \
+     -systemPassword "$SYSTEM_PASS" \
+     -createAsContainerDatabase true \
+     -numberOfPDBs 1 \
+     -pdbName \$PDB_NAME \
+     -pdbAdminPassword "$PDBADMIN_PASS" \
+     -databaseType MULTIPURPOSE \
+     -automaticMemoryManagement false \
+     -totalMemory 1500 \
+     -storageType FS \
+     -datafileDestination "$ORACLE_DB" \
+     -redoLogFileSize 50 \
+     -emConfiguration NONE \
+     -ignorePreReqs" > ${SCRIPT_DIR}/run_dbca
+chmod a+x ${SCRIPT_DIR}/run_dbca
+su - $O_USER -c ${SCRIPT_DIR}/run_dbca
+rm -f ${SCRIPT_DIR}/run_dbca
+
+# Change auto start flag from N to Y
+sed -e "/$CDB/s/^/#/g" $ORATAB > ${SCRIPT_DIR}/tmporatab
+grep "$CDB.*:N" $ORATAB | sed s'/..$/:Y/' >> ${SCRIPT_DIR}/tmporatab
+cat ${SCRIPT_DIR}/tmporatab > $ORATAB
+rm -f ${SCRIPT_DIR}/tmporatab
