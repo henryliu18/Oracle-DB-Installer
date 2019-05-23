0386ad91dd80f38067d02f4202d592f86912376b
henryliu18 <45472005+henryliu18@users.noreply.github.com>
2019-05-23 12:56:27 +1000

    Update linux.sh

--- 8.1.7/linux.sh
+++ 8.1.7/linux.sh
@@ -41,113 +41,135 @@ else
  exit 1
 fi
 
-
-## Execute below command to allow root ftp or simply remove "root" from /etc/ftpusers
-#if [ ! -f /etc/ftpusers.orig ]; then
-#  cp /etc/ftpusers /etc/ftpusers.orig
-#  sed -e '/root/s/^/#/g' /etc/ftpusers.orig > /etc/ftpusers
-#fi
-
-## Execute below command to allow root telnet or simply comment out "auth       required    /lib/security/pam_securetty.so" from /etc/pam.d/login
-#if [ ! -f /etc/pam.d/login.orig ]; then
-#  cp /etc/pam.d/login /etc/pam.d/login.orig
-#  sed -e '/pam_securetty.so/s/^/#/g' /etc/pam.d/login.orig > /etc/pam.d/login
-#fi
-
-## Only if telnetd and ftpd not up
-#if [ -d "/mnt/cdrom/RedHat/RPMS" ]; then
-#  rpm -ivh /mnt/cdrom/RedHat/RPMS/telnet-server-0.16-6.i386.rpm
-#  rpm -ivh /mnt/cdrom/RedHat/RPMS/wu-ftpd-2.6.0-3.i386.rpm
-#  rpm -ivh /mnt/cdrom/RedHat/RPMS/inetd-0.16-4.i386.rpm
-#  /etc/rc.d/init.d/inet start
-#  chkconfig inet on
-#else
-#  echo "RedHat iso not mounted"
-#  exit 1
-#fi
-
-#
-# Database software installation, run as root user
-#
-
-#JAVA_HOME=/usr/local/java
-#JAVA_SW=/tmp/jdk-1_2_2_017-linux-i586.tar.gz
-#ORACLE_SW1=/tmp/linux81701.tar.bz2
-#XMING_IP=192.168.56.1
-
-# user and groups creation
-groupadd -g 54321 oinstall
-groupadd -g 54322 dba
-useradd -u 54321 -g oinstall -G dba $O_USER
-
-#Specify oracle password
-passwd $O_USER <<EOF
-$O_PASS
-$O_PASS
-EOF
-
-# Only if firewall is on
-/etc/rc.d/init.d/ipchains stop
-chkconfig ipchains off
-
-if [ -f "$JAVA_SW" ]; then
-  cd /usr/local
-  tar xvfz $JAVA_SW
-  ln -s jdk1.2.2 java
-else
-  echo "JAVA Software not found - $JAVA_SW"
-  exit 1
-fi
-
-echo "unset USERNAME
-ORACLE_BASE=$ORACLE_BASE
-JAVA_HOME=$JAVA_HOME
-LC_ALL=C
-LANG=C
-NLS_LANG=American_America.UTF8
-ORA_NLS33=$ORACLE_HOME/ocommon/nls/admin/data
-export ORACLE_BASE JAVA_HOME LC_ALL LANG NLS_LANG ORA_NLS33
-ORACLE_HOME=$ORACLE_HOME
-PATH=$JAVA_HOME/bin:$ORACLE_HOME/bin:$PATH
-export ORACLE_HOME PATH" >> /home/$O_USER/.bashrc
-
-#Create directories for software and database
-mkdir -p $ORACLE_HOME
-mkdir -p $ORACLE_DB/data001
-mkdir -p $ORACLE_DB/dbfra001
-mkdir -p $ORACLE_DB/redo001
-mkdir -p $ORACLE_DB/redo002
-
-chown -R $O_USER:oinstall $ORACLE_BASE $ORACLE_DB
-chmod -R 775 $ORACLE_BASE $ORACLE_DB
-
-#echo "oracle soft nofile 65536
-#oracle hard nofile 65536
-#oracle soft nproc 16384
-#oracle hard nproc 16384" >> /etc/security/limits.conf
-
-
-# Required packages
-cd /mnt/cdrom/RedHat/RPMS/
-rpm -ivh \
-XFree86-3.3.6-20.i386.rpm \
-XFree86-libs-3.3.6-20.i386.rpm \
-XFree86-xfs-3.3.6-20.i386.rpm \
-make-3.78.1-4.i386.rpm \
-egcs-1.1.2-30.i386.rpm \
-cpp-1.1.2-30.i386.rpm \
-glibc-devel-2.1.3-15.i386.rpm \
-kernel-headers-2.2.14-5.0.i386.rpm
-
-# Making shell script for oracle installer
-echo "mkdir $ORACLE_SW_STG
-cd $ORACLE_SW_STG
-bunzip2 -cd $ORACLE_SW1 | tar xvf -
+# XMING checks
 export DISPLAY=$XMING_IP:0.0
-$ORACLE_SW_STG/Disk1/runInstaller" > $SCRIPT_DIR/inst_ora_sw
+(xdpyinfo>/dev/null) &
+my_pid=$!
 
-# Adding execute permission to all users
-chmod a+x $SCRIPT_DIR/inst_ora_sw
+i=0
+while   ps | grep " $my_pid ">/dev/null     # might also need  | grep -v grep  here
+do
+    i=$((i+1))
+    #echo $my_pid is still in the ps output. Must still be running "$i".
+    sleep 2
+    if [ $i -eq 3 ]; then
+      #echo "timeout killing $my_pid"
+      timeout=yes
+      kill -9 $my_pid
+    fi
+done
 
-# unzip; set DISPLAY; runInstaller as oracle
-su - $O_USER -c $SCRIPT_DIR/inst_ora_sw
+if [ "$timeout" = "yes" ]; then
+  echo "exiting, check XMING or firewall"
+  exit 1
+else
+  ## Execute below command to allow root ftp or simply remove "root" from /etc/ftpusers
+  #if [ ! -f /etc/ftpusers.orig ]; then
+  #  cp /etc/ftpusers /etc/ftpusers.orig
+  #  sed -e '/root/s/^/#/g' /etc/ftpusers.orig > /etc/ftpusers
+  #fi
+
+  ## Execute below command to allow root telnet or simply comment out "auth       required    /lib/security/pam_securetty.so" from /etc/pam.d/login
+  #if [ ! -f /etc/pam.d/login.orig ]; then
+  #  cp /etc/pam.d/login /etc/pam.d/login.orig
+  #  sed -e '/pam_securetty.so/s/^/#/g' /etc/pam.d/login.orig > /etc/pam.d/login
+  #fi
+
+  ## Only if telnetd and ftpd not up
+  #if [ -d "/mnt/cdrom/RedHat/RPMS" ]; then
+  #  rpm -ivh /mnt/cdrom/RedHat/RPMS/telnet-server-0.16-6.i386.rpm
+  #  rpm -ivh /mnt/cdrom/RedHat/RPMS/wu-ftpd-2.6.0-3.i386.rpm
+  #  rpm -ivh /mnt/cdrom/RedHat/RPMS/inetd-0.16-4.i386.rpm
+  #  /etc/rc.d/init.d/inet start
+  #  chkconfig inet on
+  #else
+  #  echo "RedHat iso not mounted"
+  #  exit 1
+  #fi
+
+  #
+  # Database software installation, run as root user
+  #
+
+  #JAVA_HOME=/usr/local/java
+  #JAVA_SW=/tmp/jdk-1_2_2_017-linux-i586.tar.gz
+  #ORACLE_SW1=/tmp/linux81701.tar.bz2
+  #XMING_IP=192.168.56.1
+
+  # user and groups creation
+  groupadd -g 54321 oinstall
+  groupadd -g 54322 dba
+  useradd -u 54321 -g oinstall -G dba $O_USER
+
+  #Specify oracle password
+  passwd $O_USER <<EOF
+  $O_PASS
+  $O_PASS
+  EOF
+
+  # Only if firewall is on
+  /etc/rc.d/init.d/ipchains stop
+  chkconfig ipchains off
+
+  if [ -f "$JAVA_SW" ]; then
+    cd /usr/local
+    tar xvfz $JAVA_SW
+    ln -s jdk1.2.2 java
+  else
+    echo "JAVA Software not found - $JAVA_SW"
+    exit 1
+  fi
+
+  echo "unset USERNAME
+  ORACLE_BASE=$ORACLE_BASE
+  JAVA_HOME=$JAVA_HOME
+  LC_ALL=C
+  LANG=C
+  NLS_LANG=American_America.UTF8
+  ORA_NLS33=$ORACLE_HOME/ocommon/nls/admin/data
+  export ORACLE_BASE JAVA_HOME LC_ALL LANG NLS_LANG ORA_NLS33
+  ORACLE_HOME=$ORACLE_HOME
+  PATH=$JAVA_HOME/bin:$ORACLE_HOME/bin:$PATH
+  export ORACLE_HOME PATH" >> /home/$O_USER/.bashrc
+
+  #Create directories for software and database
+  mkdir -p $ORACLE_HOME
+  mkdir -p $ORACLE_DB/data001
+  mkdir -p $ORACLE_DB/dbfra001
+  mkdir -p $ORACLE_DB/redo001
+  mkdir -p $ORACLE_DB/redo002
+
+  chown -R $O_USER:oinstall $ORACLE_BASE $ORACLE_DB
+  chmod -R 775 $ORACLE_BASE $ORACLE_DB
+
+  #echo "oracle soft nofile 65536
+  #oracle hard nofile 65536
+  #oracle soft nproc 16384
+  #oracle hard nproc 16384" >> /etc/security/limits.conf
+
+
+  # Required packages
+  cd /mnt/cdrom/RedHat/RPMS/
+  rpm -ivh \
+  XFree86-3.3.6-20.i386.rpm \
+  XFree86-libs-3.3.6-20.i386.rpm \
+  XFree86-xfs-3.3.6-20.i386.rpm \
+  make-3.78.1-4.i386.rpm \
+  egcs-1.1.2-30.i386.rpm \
+  cpp-1.1.2-30.i386.rpm \
+  glibc-devel-2.1.3-15.i386.rpm \
+  kernel-headers-2.2.14-5.0.i386.rpm
+
+  # Making shell script for oracle installer
+  echo "mkdir $ORACLE_SW_STG
+  cd $ORACLE_SW_STG
+  bunzip2 -cd $ORACLE_SW1 | tar xvf -
+  export DISPLAY=$XMING_IP:0.0
+  $ORACLE_SW_STG/Disk1/runInstaller" > $SCRIPT_DIR/inst_ora_sw
+
+  # Adding execute permission to all users
+  chmod a+x $SCRIPT_DIR/inst_ora_sw
+
+  # unzip; set DISPLAY; runInstaller as oracle
+  su - $O_USER -c $SCRIPT_DIR/inst_ora_sw
+fi
