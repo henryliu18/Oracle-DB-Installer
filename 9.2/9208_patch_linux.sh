81e398b8b761fdc43d2b270c0af73fb856825833
henryliu18 <45472005+henryliu18@users.noreply.github.com>
2019-05-24 17:40:25 +1000

    Update 9208_patch_linux.sh

--- 9.2/9208_patch_linux.sh
+++ 9.2/9208_patch_linux.sh
@@ -45,11 +45,13 @@ mv /usr/bin/gcc32 /usr/bin/gcc
 echo "mkdir $SCRIPT_DIR/9208
 cd $SCRIPT_DIR/9208
 unzip /tmp/p4547809_92080_Linux-x86-64.zip
-$SCRIPT_DIR/9208/Disk1/runInstaller -silent -responseFile NO_VALUE \
-UNIX_GROUP_NAME=\"oinstall\"
+$SCRIPT_DIR/9208/Disk1/runInstaller -silent \
+UNIX_GROUP_NAME=\"oinstall\" \
 FROM_LOCATION="$SCRIPT_DIR/9208/Disk1/stage/products.xml" \
-ORACLE_HOME=\$ORACLE_HOME \
-ORACLE_HOME_NAME=\"OraHome92\"
+ORACLE_HOME=$ORACLE_HOME \
+ORACLE_HOME_NAME=\"OraHome92\" \
+TOPLEVEL_COMPONENT={\"oracle.server\",\"9.2.0.8.0\"} \
+NEXT_SESSION=true
 " > ${SCRIPT_DIR}/inst_ora_sw
 
 # Adding execute permission to all users
