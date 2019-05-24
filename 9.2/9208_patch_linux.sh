3879abcf9cc4aa0f2e2c4e1f8492b21ae2923f8f
henryliu18 <45472005+henryliu18@users.noreply.github.com>
2019-05-24 19:26:18 +1000

    Update 9208_patch_linux.sh

--- 9.2/9208_patch_linux.sh
+++ 9.2/9208_patch_linux.sh
@@ -10,33 +10,7 @@ else
  exit 1
 fi
 
-# XMING checks
-export DISPLAY=$XMING_IP:0.0
-(xdpyinfo>/tmp/xtest 2>&1) &
-my_pid=$!
 
-i=0
-while   ps | grep " $my_pid ">/dev/null     # might also need  | grep -v grep  here
-do
-    i=$((i+1))
-    #echo $my_pid is still in the ps output. Must still be running "$i".
-    sleep 2
-    if [ $i -eq 3 ]; then
-      #echo "timeout killing $my_pid"
-      timeout=yes
-      kill -9 $my_pid
-    fi
-done
-
-if [ "$timeout" = "yes" ]; then
-  echo "exiting, check XMING or firewall or X0.hosts file"
-  exit 1
-else
-  value=$( grep -ic "refused" /tmp/xtest )
-  if [ "$value" -eq 1 ]; then
-    echo "exiting, check XMING or firewall or X0.hosts file"
-    exit 1
-  else
 # do patching here
 # Replace gcc 3.4 with gcc 3.2 - root task
 mv /usr/bin/gcc /usr/bin/gcc34
@@ -62,5 +36,3 @@ su - $O_USER -c ${SCRIPT_DIR}/inst_ora_sw
 # Revert gcc 3.4 from gcc 3.2
 mv /usr/bin/gcc /usr/bin/gcc32
 mv /usr/bin/gcc34 /usr/bin/gcc
-  fi
-fi
