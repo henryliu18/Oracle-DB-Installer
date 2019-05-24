d5a5103990c3064ba51e870e1d784bb5f1da1d16
henryliu18 <45472005+henryliu18@users.noreply.github.com>
2019-05-24 17:22:32 +1000

    Update 9208_patch_linux.sh

--- 9.2/9208_patch_linux.sh
+++ 9.2/9208_patch_linux.sh
@@ -42,12 +42,12 @@ else
 mv /usr/bin/gcc /usr/bin/gcc34
 mv /usr/bin/gcc32 /usr/bin/gcc
 
-echo "mkdir /tmp/9208
-cd /tmp/9208
+echo "mkdir $SCRIPT_DIR/9208
+cd SCRIPT_DIR/9208
 unzip /tmp/p4547809_92080_Linux-x86-64.zip
-/tmp/9208/Disk1/runInstaller -silent -responseFile NO_VALUE \
+$SCRIPT_DIR/9208/Disk1/runInstaller -silent -responseFile NO_VALUE \
 UNIX_GROUP_NAME=\"oinstall\"
-FROM_LOCATION="/tmp/9208/Disk1/stage/products.xml" \
+FROM_LOCATION="$SCRIPT_DIR/9208/Disk1/stage/products.xml" \
 ORACLE_HOME=$ORACLE_HOME \
 ORACLE_HOME_NAME=\"OraHome92\"
 " > ${SCRIPT_DIR}/inst_ora_sw
