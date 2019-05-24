0e0f106015b3b57b5417cc9aad727dffb4c4ac03
henryliu18 <45472005+henryliu18@users.noreply.github.com>
2019-05-24 13:25:18 +1000

    Update 8174_patch_linux.sh

--- 8.1.7/8174_patch_linux.sh
+++ 8.1.7/8174_patch_linux.sh
@@ -22,6 +22,33 @@ else
  exit 1
 fi
 
+# XMING checks
+export DISPLAY=$XMING_IP:0.0
+(xdpyinfo>/tmp/xtest 2>&1) &
+my_pid=$!
+
+i=0
+while   ps | grep " $my_pid ">/dev/null     # might also need  | grep -v grep  here
+do
+    i=$((i+1))
+    #echo $my_pid is still in the ps output. Must still be running "$i".
+    sleep 2
+    if [ $i -eq 3 ]; then
+      #echo "timeout killing $my_pid"
+      timeout=yes
+      kill -9 $my_pid
+    fi
+done
+
+if [ "$timeout" = "yes" ]; then
+  echo "exiting, check XMING or firewall or X0.hosts file"
+  exit 1
+else
+  value=$( grep -ic "refused" /tmp/xtest )
+  if [ "$value" -eq 1 ]; then
+    echo "exiting, check XMING or firewall or X0.hosts file"
+    exit 1
+  else
 # Making shell script for oracle installer
 echo "mkdir $ORACLE_SW_STG
 cd $ORACLE_SW_STG
@@ -37,3 +64,5 @@ su - $O_USER -c $SCRIPT_DIR/inst_ora_sw
 
 # cleanup
 rm -f $SCRIPT_DIR/inst_ora_sw
+  fi
+fi
