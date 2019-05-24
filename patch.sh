f2e42c96b8c2617939880ba1178297e0c46bf678
henryliu18 <45472005+henryliu18@users.noreply.github.com>
2019-05-24 17:20:28 +1000

    Create patch.sh

---
+++ patch.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Source env
+if [ -f `dirname $0`/env ]; then
+ . `dirname $0`/env
+else
+ echo "env file not found in `dirname $0`, run setup to create env file"
+ echo "cd `dirname $0`;bash `dirname $0`/setup env"
+ exit 1
+fi
+
+if [ "$O_VER" = "18c" ]; then
+  #bash `dirname $0`/18c/patch.sh
+elif [ "$O_VER" = "12c" ]; then
+  bash `dirname $0`/12c/patch.sh
+elif [ "$O_VER" = "11.2" ]; then
+  bash `dirname $0`/11.2/patch.sh
+elif [ "$O_VER" = "10.2" ]; then
+  bash `dirname $0`/10.2/patch.sh
+elif [ "$O_VER" = "9.2" ]; then
+  bash `dirname $0`/9.2/9208_patch_linux.sh && $ORACLE_HOME/root.sh
+elif [ "$O_VER" = "8.1.7" ]; then
+  bash `dirname $0`/8.1.7/patch.sh
+else
+  echo "unknown version"
+  exit 2;
+fi
