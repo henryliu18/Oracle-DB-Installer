97848ff3e44dec651cbff4b3988e98e1df8a50b6
henryliu18 <45472005+henryliu18@users.noreply.github.com>
2019-05-24 11:55:18 +1000

    Update dbca.sh

--- 9.2/dbca.sh
+++ 9.2/dbca.sh
@@ -40,15 +40,17 @@ else
     echo "exiting, check XMING or firewall or X0.hosts file"
     exit 1
   else
-echo "dbca -silent -createDatabase -templateName General_Purpose.dbc \
--gdbname \$ORACLE_SID -sid \$ORACLE_SID -responseFile NO_VALUE -characterSet AL32UTF8 \
--memoryPercentage 10 -emConfiguration NONE -datafiledestination $ORACLE_DB \
--sysPassword $SYS_PASS -systemPassword $SYSTEM_PASS \
--dbsnmpPassword $SYSTEM_PASS -sysmanPassword $SYSTEM_PASS" > ${SCRIPT_DIR}/run_dbca
+echo "dbca -silent -createDatabase -templateName General_Purpose.dbc -gdbname $ORACLE_SID -sid $ORACLE_SID \
+-datafileJarLocation $ORACLE_HOME/assistants/dbca/templates -datafileDestination $ORACLE_DB -responseFile NO_VALUE \
+-characterSet AL32UTF8" > ${SCRIPT_DIR}/run_dbca
 chmod a+x ${SCRIPT_DIR}/run_dbca
 su - $O_USER -c ${SCRIPT_DIR}/run_dbca
 rm -f ${SCRIPT_DIR}/run_dbca
 
 # Change auto start flag from N to Y
+sed -e "/$CDB/s/^/#/g" $ORATAB > ${SCRIPT_DIR}/tmporatab
+grep "$CDB.*:N" $ORATAB | sed s'/..$/:Y/' >> ${SCRIPT_DIR}/tmporatab
+cat ${SCRIPT_DIR}/tmporatab > $ORATAB
+rm -f ${SCRIPT_DIR}/tmporatab
   fi
 fi
