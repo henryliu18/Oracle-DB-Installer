ee9a5884a3535bfad8df36c9e2f9d93d3b0912d7
henryliu18 <45472005+henryliu18@users.noreply.github.com>
2019-05-29 01:41:36 +1000

    Update linux_x64.sh

--- 12c/linux_x64.sh
+++ 12c/linux_x64.sh
@@ -21,24 +21,8 @@ source function.sh
 #/etc/hosts configuration
 echo "`ip -f inet addr show $NIC | grep -Po 'inet \K[\d.]+'` `hostname`" >> /etc/hosts
 
-#Configuring the Kernel Parameter Settings
-echo "fs.file-max = 6815744
-kernel.sem = 250 32000 100 128
-kernel.shmmni = 4096
-kernel.shmall = 1073741824
-kernel.shmmax = 4398046511104
-kernel.panic_on_oops = 1
-net.core.rmem_default = 262144
-net.core.rmem_max = 4194304
-net.core.wmem_default = 262144
-net.core.wmem_max = 1048576
-net.ipv4.conf.all.rp_filter = 2
-net.ipv4.conf.default.rp_filter = 2
-fs.aio-max-nr = 1048576
-net.ipv4.ip_local_port_range = 9000 65500" >> /etc/sysctl.conf
-
-#Run the following command to change the current kernel parameters.
-/sbin/sysctl -p
+# Kernel parameters tuning
+kernel_params $O_VER
 
 #Setting Shell Limits for the Oracle User
 echo "$O_USER   soft   nofile    1024
