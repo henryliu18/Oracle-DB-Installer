dc61b6337a59a7b632c9ace2dac73506dc37cb94
henryliu18 <45472005+henryliu18@users.noreply.github.com>
2019-05-28 23:11:40 +1000

    Update function.sh

--- function.sh
+++ function.sh
@@ -1,3 +1,36 @@
 lines_in_file () {
 cat $1 | wc -l
 }
+
+xming_check () {
+# XMING checks
+export DISPLAY=$XMING_IP:0.0
+(xdpyinfo>/tmp/xtest 2>&1) &
+my_pid=$!
+
+i=0
+while   ps | grep $my_pid>/dev/null     # might also need  | grep -v grep  here
+do
+    i=$((i+1))
+    #echo $my_pid is still in the ps output. Must still be running "$i".
+    sleep 2
+    if [ $i -eq 3 ]; then
+      #echo "timeout killing $my_pid"
+      timeout=yes
+      kill -9 $my_pid
+    fi
+done
+
+if [ "$timeout" = "yes" ]; then
+  #echo "exiting, check XMING or firewall or X0.hosts file"
+  return 1
+else
+  value=$( grep -ic "refused" /tmp/xtest )
+  if [ "$value" -eq 1 ]; then
+    #echo "exiting, check XMING or firewall or X0.hosts file"
+    return 1
+  else
+    return 0
+  fi
+fi
+}
