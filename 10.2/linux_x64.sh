f12cc4e9e76a6de08ab15b2f06368fcecd3688c7
henryliu18 <45472005+henryliu18@users.noreply.github.com>
2019-05-29 01:13:16 +1000

    Update linux_x64.sh

--- 10.2/linux_x64.sh
+++ 10.2/linux_x64.sh
@@ -141,14 +141,7 @@ if [ $USER = "$O_USER" ]; then
 fi" >> /home/$O_USER/.bash_profile
 
 #Create directories for software and database
-mkdir -p $ORACLE_HOME
-mkdir -p $ORACLE_DB/data001
-mkdir -p $ORACLE_DB/dbfra001
-mkdir -p $ORACLE_DB/redo001
-mkdir -p $ORACLE_DB/redo002
-
-chown -R $O_USER:oinstall $ORACLE_BASE $ORACLE_DB
-chmod -R 775 $ORACLE_BASE $ORACLE_DB
+cr_directories
 
 echo "$O_USER soft nproc 2047
 $O_USER hard nproc 16384
