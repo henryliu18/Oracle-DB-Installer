7621d1fbb5aa73bccc62cb0df7f7e8fe58a47729
henryliu18 <45472005+henryliu18@users.noreply.github.com>
2019-05-31 14:55:53 +1000

    Update linux_x64.sh

--- 18c/linux_x64.sh
+++ 18c/linux_x64.sh
@@ -99,20 +99,7 @@ $O_PASS
 EOF
 
 #Set secure Linux to permissive
-echo "# This file controls the state of SELinux on the system.
-# SELINUX= can take one of these three values:
-#     enforcing - SELinux security policy is enforced.
-#     permissive - SELinux prints warnings instead of enforcing.
-#     disabled - No SELinux policy is loaded.
-#SELINUX=enforcing
-SELINUX=permissive
-# SELINUXTYPE= can take one of three two values:
-#     targeted - Targeted processes are protected,
-#     minimum - Modification of targeted policy. Only selected processes are protected.
-#     mls - Multi Level Security protection.
-SELINUXTYPE=targeted" > /etc/selinux/config
-
-setenforce Permissive
+selinux_mode permissive
 
 #Stop and disable firewalld
 firewall_off
