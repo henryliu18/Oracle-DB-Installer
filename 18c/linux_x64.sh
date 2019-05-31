37500aab4dba1e6b0010cfc4233699159afe0520
henryliu18 <45472005+henryliu18@users.noreply.github.com>
2019-05-31 15:09:56 +1000

    Update linux_x64.sh

--- 18c/linux_x64.sh
+++ 18c/linux_x64.sh
@@ -85,18 +85,8 @@ yum install -y unixODBC
 yum install -y unzip
 yum install -y gcc-c++
 
-#Create the new groups and users
-groupadd -g 54321 oinstall
-groupadd -g 54322 dba
-groupadd -g 54323 oper
-
-useradd -u 54321 -g oinstall -G dba,oper $O_USER
-
-#Specify oracle password
-passwd $O_USER <<EOF
-$O_PASS
-$O_PASS
-EOF
+#oracle user and groups creation
+cr_user_and_groups
 
 #Set secure Linux to permissive
 selinux_mode permissive
